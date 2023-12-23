4d5cdc1bc26d97f0c885dc288d134520861c724c
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2023-12-23 13:08:36 +0700

    gin path variable

--- simple-web-app/main.go
+++ simple-web-app/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -29,6 +31,12 @@ func main() {
 	// Menambahkan buku
 	router.POST("/books/create", createBook)
 
+	// Menampilhkan detail buku
+	router.GET("/books/:id", getBookById)
+
+	// Update buku
+	router.PUT("/books/:id", updateBookById)
+
 	err := router.Run(":8080")
 	if err != nil {
 		panic(err)
@@ -50,4 +58,61 @@ func createBook(c *gin.Context) {
 
 	books = append(books, newBook)
 	c.JSON(http.StatusCreated, newBook)
+}
+
+func getBookById(c *gin.Context) {
+	// save value from path variable
+	id := c.Param("id")
+
+	bookId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid book id"})
+		return
+	}
+
+	for _, book := range books {
+		if book.Id == bookId {
+			c.JSON(http.StatusOK, book)
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"error": "Book not found!"})
+}
+
+func updateBookById(c *gin.Context) {
+	id := c.Param("id")
+
+	bookId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid book id"})
+		return
+	}
+
+	var updatedBook Book
+
+	if err := c.ShouldBind(&updatedBook); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	for i, book := range books {
+		if book.Id == bookId {
+			if strings.TrimSpace(updatedBook.Title) != "" {
+				books[i].Title = updatedBook.Title
+			}
+			if strings.TrimSpace(updatedBook.Author) != "" {
+				books[i].Author = updatedBook.Author
+			}
+			if strings.TrimSpace(updatedBook.ReleaseYear) != "" {
+				books[i].ReleaseYear = updatedBook.ReleaseYear
+			}
+			if updatedBook.Pages != 0 {
+				books[i].Pages = updatedBook.Pages
+			}
+			c.JSON(http.StatusOK, gin.H{"message": "Book updated", "data": books[i]})
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
 }
