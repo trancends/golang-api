1217a9973db19882b4c52af6413ea7022def3faf
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2023-12-23 21:20:39 +0700

    api and db

---
+++ simple-web-app-with-db/config/database-connection.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "trancendsdb@3821"
+	dbName   = "BookDb"
+)
+
+func ConnectDB() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Coonected to Database")
+	return db
+}
--- simple-web-app-with-db/main.go
+++ simple-web-app-with-db/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"net/http"
+	"simple-web-app-with-db/config"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +15,8 @@ type Book struct {
 	ReleaseYear string `json:"releaseYear"`
 	Pages       int    `json:"pages"`
 }
+
+var db = config.ConnectDB()
 
 func main() {
 	router := gin.Default()
@@ -26,10 +32,65 @@ func main() {
 
 // Handler untuk membuat buku baru
 func createBook(c *gin.Context) {
+	var newBook Book
+	err := c.ShouldBind(&newBook)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	query := "INSERT INTO mst_book (title, author, release_year, pages) VALUES ($1, $2, $3, $4) RETURNING id"
+
+	var bookId int
+	err = db.QueryRow(query, newBook.Title, newBook.Author, newBook.ReleaseYear, newBook.Pages).Scan(&bookId)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create book"})
+		return
+	}
+
+	newBook.ID = bookId
+	c.JSON(http.StatusCreated, newBook)
 }
 
 // Handler untuk menampilkan semua buku atau buku berdasarkan pencarian judul
 func getAllBooks(c *gin.Context) {
+	searchTitle := c.Query("title")
 
+	query := "SELECT id,title,author,release_year,pages FROM mst_book"
+
+	var rows *sql.Rows
+	var err error
+
+	if searchTitle != "" {
+		query += " WHERE title ILIKE '%' || $1 || '%'"
+		rows, err = db.Query(query, searchTitle)
+	} else {
+		rows, err = db.Query(query)
+	}
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	defer rows.Close()
+
+	var matchedBooks []Book
+	for rows.Next() {
+		var book Book
+		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.ReleaseYear, &book.Pages)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+			return
+		}
+
+		matchedBooks = append(matchedBooks, book)
+	}
+
+	if len(matchedBooks) > 0 {
+		c.JSON(http.StatusOK, matchedBooks)
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
+	}
 }
