1a5834950e233021f04fc1e03a39adaef7198304
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2023-12-28 20:46:39 +0700

    modify simple-web-app-db and add starter for submission project

--- simple-web-app-with-db/main.go
+++ simple-web-app-with-db/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"net/http"
 	"simple-web-app-with-db/config"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -26,6 +27,9 @@ func main() {
 
 	// Menampilkan semua buku
 	router.GET("/books", getAllBooks)
+
+	// Update buku
+	router.PUT("/books", updateBookById)
 
 	router.Run(":8080")
 }
@@ -51,6 +55,57 @@ func createBook(c *gin.Context) {
 
 	newBook.ID = bookId
 	c.JSON(http.StatusCreated, newBook)
+}
+
+func updateBookById(c *gin.Context) {
+	id := c.Param("id")
+	bookId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	var updateBook Book
+
+	if err := c.ShouldBind(&updateBook); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	// update book by id
+	// Retrieve the current book details from the database
+	var currentBook Book
+	query := "SELECT id, title, author, release_year, pages FROM mst_book WHERE id = $1"
+	err = db.QueryRow(query, bookId).Scan(&currentBook.ID, &currentBook.Title, &currentBook.Author, &currentBook.ReleaseYear, &currentBook.Pages)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve book"})
+		}
+		return
+	}
+
+	// Update fields if they are provided in the request
+	if updateBook.Title != "" {
+		currentBook.Title = updateBook.Title
+	}
+	if updateBook.Author != "" {
+		currentBook.Author = updateBook.Author
+	}
+	if updateBook.ReleaseYear != "" {
+		currentBook.ReleaseYear = updateBook.ReleaseYear
+	}
+	if updateBook.Pages != 0 {
+		currentBook.Pages = updateBook.Pages
+	}
+
+	// Update the book details in the database
+	updateQuery := `UPDATE mst_book SET title = $2, author = $3, release_year = $4, pages = $5 WHERE id = $1`
+	_, err = db.Exec(updateQuery, currentBook.ID, currentBook.Title, currentBook.Author, currentBook.ReleaseYear, currentBook.Pages)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update book"})
+		return
+	}
+
+	c.JSON(http.StatusOK, currentBook)
 }
 
 // Handler untuk menampilkan semua buku atau buku berdasarkan pencarian judul
